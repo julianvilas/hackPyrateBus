49cc1d5055dd3a7b3595a72a0998e06877af7dc8
Julian Vilas <julian.vilas@gmail.com>
2024-06-01 23:11:16 +0200

    workflows/release.yaml: convert to manual
    Use it for auto-increase release version and create release.

--- hackPyrateBus/__init__.py
+++ hackPyrateBus/__init__.py
@@ -1,3 +1,3 @@
 from .AT24CXXX import *
 
-__version__ = '0.0.5'
+__version__ = '0.0.6-alpha.1'
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name = "hackPyrateBus",
-    version = "0.0.5",
+    version = '0.0.6-alpha.1',
     python_requires='>=3.11',
     description = ("Python library with high-level functions to interact with specific ICs using Bus Pirate"),
     license = "GPLv3",
