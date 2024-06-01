d5136e8f415ed3e28e75dcaac45c464619e0c3fb
Julian Vilas <julian.vilas@gmail.com>
2024-06-01 11:04:01 +0200

    all: adapt documentation about the vendoring

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -13,11 +13,15 @@ setup(
     license = "GPLv3",
     keywords = "BusPirate",
     url = "https://github.com/julianvilas/bphelpers",
-    packages=['bphelpers'],
+    # NOTE: the pyBusPirateLite dependency is vendored from
+    # https://github.com/julianvilas/pyBusPirateLite/tree/new for convenience,
+    # as the project does not seem to be maintained and it is not available in
+    # PyPI.  Again many thanks to the original author!
+    packages=find_packages(),
+    install_requires=[
+        'pyserial',
+    ],
     long_description=read('README.md'),
-    # NOTE: for convenience the pyBusPirateLite dependency is installed from a
-    # forked repository as the original one is not updated
-    install_requires=['pyBusPirateLite @ https://github.com/julianvilas/pyBusPirateLite/tarball/master'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
