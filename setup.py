adfdfa527e1930a8260393ec78a81415d440c774
Julian Vilas <julian.vilas@gmail.com>
2024-06-01 22:25:36 +0200

    all: release v0.0.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name = "hackPyrateBus",
-    version = "0.0.1",
+    version = "0.0.5",
     python_requires='>=3.11',
     description = ("Python library with high-level functions to interact with specific ICs using Bus Pirate"),
     license = "GPLv3",
